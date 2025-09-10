0b94d2fbeee9359faae8e23d129934b980c4766f
G Venkata Rama Krishna Prasad <glps369@gmail.com>
2025-09-10 19:57:48 +0530

    resource aws_lb_listener, pub_subnet_cidrs reduced in tfvars ,datasource az updated in submodules subnets

--- modules/aws_loadbalancer/main.tf
+++ modules/aws_loadbalancer/main.tf
@@ -1,9 +1,18 @@
 resource "aws_lb_target_group" "alb_target_gp" {
   name        = "${var.department}-alb-tg"
-  target_type = "alb"
+  target_type = "instance"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 resource "aws_lb" "alb" {
   name               = "${var.department}-alb"
@@ -17,9 +26,19 @@ resource "aws_lb" "alb" {
     enabled = true
   }
 }
-resource "aws_lb_target_group_attachment" "alb_attachment" {
+/* resource "aws_lb_target_group_attachment" "alb_attachment" {
   count            = length(var.aws_instance_ids)
   target_group_arn = aws_lb_target_group.alb_target_gp.arn
   target_id        = var.aws_instance_ids[count.index]
   port             = 80
+} */
+resource "aws_lb_listener" "alb_listner" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_gp.arn
+  }
 }
--- modules/aws_subnets/main.tf
+++ modules/aws_subnets/main.tf
@@ -1,13 +1,20 @@
+data "aws_availability_zones" "az" {
+  state = "available"
+
+  # exclude or blacklist particular az
+  # exclude_names = ["ap-south-1b"]
+}
 resource "aws_subnet" "public_subnets" {
   count                   = length(var.public_subnet_cidrs)
   cidr_block              = element(var.public_subnet_cidrs, count.index)
   map_public_ip_on_launch = true
   vpc_id                  = var.vpc_id
-  depends_on              = [var.vpc_id]
+  availability_zone       = data.aws_availability_zones.az.names[count.index]
   tags = {
     Name   = "${var.department}-pubsubnet ${count.index + 1}"
     subnet = "Public Subnet-${count.index + 1}"
   }
+  depends_on = [var.vpc_id]
 }
 
 resource "aws_subnet" "private_subnets" {
@@ -15,9 +22,10 @@ resource "aws_subnet" "private_subnets" {
   cidr_block              = element(var.private_subnet_cidrs, count.index)
   map_public_ip_on_launch = false
   vpc_id                  = var.vpc_id
-  depends_on              = [var.vpc_id]
+  availability_zone       = data.aws_availability_zones.az.names[count.index]
   tags = {
     Name   = "${var.department}-pvtsubnet ${count.index + 1}"
     subnet = "Private Subnet-${count.index + 1}"
   }
+  depends_on = [var.vpc_id]
 }
