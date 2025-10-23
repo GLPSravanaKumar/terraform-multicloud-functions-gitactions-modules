96710318dcf3a2d987cef342c19395e6e5fcabde
G Venkata Rama Krishna Prasad <glps369@gmail.com>
2025-10-24 00:35:54 +0530

    scripts break,continue.sh added

---
+++ scripts/continue.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set +x
+regions=(ap-south-2 us-east-1 us-west-1 ap-south-1 eu-north-1 eu-west-3)
+
+for region in "${regions[@]}"
+do
+  echo "#### AWS Global Available Regions are: $region ####"
+  vpcs=$(aws ec2 describe-vpcs --region $region --query Vpcs[].VpcId --output text )
+  if [ -n "$vpcs" ]         # true if value is not zero
+  then
+    for vpc in $vpcs
+    do
+      echo " $vpc "
+    done
+  else
+    echo " No Vpc fund in region $region "
+    continue
+  fi
+done
